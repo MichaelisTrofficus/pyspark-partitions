6a1035bb0d55992322bffdb8fe316a662b739dfa
MichaelisTrofficus <miguel.otero.pedrido.1993@gmail.com>
2023-01-22 19:48:19 +0100

    Add new helper functions

--- pyspark_partitions/helpers.py
+++ pyspark_partitions/helpers.py
@@ -1,5 +1,8 @@
 import logging
+
+import pyspark.sql.functions as sf
 from pyspark.sql import DataFrame
+from pyspark.sql.types import IntegerType
 
 
 def count_number_of_partitions(iterator):
@@ -45,3 +48,32 @@ def df_size_in_bytes_approximate(df: DataFrame, sample_perc: float = 0.05):
     """
     sample_size_in_bytes = df_size_in_bytes_exact(df.sample(sample_perc))
     return sample_size_in_bytes / sample_perc
+
+
+def add_partition_id_column(df: DataFrame):
+    return df.withColumn("partition_id", sf.spark_partition_id())
+
+
+def get_partition_count(df: DataFrame) -> DataFrame:
+    """
+    Gets the number of registers per partition. This method is useful if we are trying to determine if some
+    partition is skewed.
+
+    :return: A DataFrame containing `partition_id` and `count` columns
+    """
+    return add_partition_id_column(df).groupBy("partition_id").count()
+
+
+def add_salt_column(df: DataFrame, skew_factor: int):
+    """
+    Adds a salt column to a DataFrame. We will be using this salt column when we are trying to perform
+    join, groupBy, etc. operations into a skewed DataFrame. The idea is to add a random column and use
+    the original keys + this salted key to perform the operations, so that we can avoid data skewness and
+    possibly, OOM errors.
+
+    :param df: A Pyspark DataFrame
+    :param skew_factor: The skew factor. For example, if we set this value to 3, then the salted column will
+        be populated by the elements 0, 1 and 2, extracted from a uniform probability distribution.
+    :return: The original DataFrame with a `salt_id` column.
+    """
+    return df.withColumn("salt_id", (sf.rand() * skew_factor).cast(IntegerType()))
